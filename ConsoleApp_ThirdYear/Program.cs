0de9bc39996c074f8979a73598b9be85507510d8
ketig <41895005+ketig@users.noreply.github.com>
2023-12-15 19:20:55 +0400

    Add project files.

---
+++ ConsoleApp_ThirdYear/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp_ThirdYear;
+
+
+Rectangle rectangle = new Rectangle(8, 15);
+rectangle.Print();
+
+Rectangle rectangle1 = new Rectangle();
+
+
+//Rectangle rectangle1 = rectangle;
+//rectangle1.Print();
+//rectangle1.Side1 = 3;
+//rectangle1 = new Rectangle(4, 5);
+//rectangle = new Rectangle(6, 7);
+
+//int x = 10;
+//int y = x;
+//y = 12;
---
+++ ConsoleApp_ThirdYear/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ThirdYear
+{
+    class Rectangle : Shape
+    {
+        private double _side1;
+        private double _side2;
+        
+        public double Side1
+        {
+            get
+            {
+                return _side1;
+            }
+            set
+            {
+                _side1 = value;
+                _perimeter = 2 * (Side1 + Side2);
+                _area = Side1 * Side2;
+            }
+        }
+        public double Side2
+        {
+            get
+            {
+                return _side2;
+            }
+            set
+            {
+                _side2 = value;
+                _perimeter = 2 * (Side1 + Side2);
+                _area = Side1 * Side2;
+            }
+        }
+        
+
+        public Rectangle() : base()
+        {
+            Console.WriteLine("Rectangle is created");
+        }
+
+        public Rectangle(double Side1, double Side2)
+        {
+            this.Side1 = Side1;
+            this.Side2 = Side2;
+            //_perimeter = 2 * (Side1 + Side2);
+            //_area = Side1 * Side2;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Side1 = {Side1}, Side2 = {Side2},");
+            Console.WriteLine($"Area = {Area}, Perimeter = {Perimeter}.\n");
+        }
+    }
+}
