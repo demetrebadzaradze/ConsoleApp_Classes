fe02dd1515843816e7f231cf46da5712a28f63b4
demetrebadzgaradze <128867020+demetrebadzaradze@users.noreply.github.com>
2024-02-13 15:56:57 +0000

    taaddaa print directories without recyrtion

---
+++ testConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] argus)
+        {
+
+        }
+        static void PrintDirectories(string rootPath)
+        {
+            Stack<string> directoryStack = new Stack<string>();
+            directoryStack.Push(rootPath);
+
+            while (directoryStack.Count > 0)
+            {
+                string currentDir = directoryStack.Pop();
+                Console.WriteLine(currentDir);
+
+                string[] subDirectories = Directory.GetDirectories(currentDir);
+                foreach (string subDir in subDirectories)
+                {
+                    directoryStack.Push(subDir);
+                }
+            }
+        }
+    }
+}
